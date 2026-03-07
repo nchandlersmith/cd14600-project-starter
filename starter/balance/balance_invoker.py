9e0ae71baf4f84735c0d0c4e02d4db8858a62b13
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2026-03-06 19:03:14 -0600

    Implement command pattern in main; enhance BalanceInvoker usage with logging and undo functionality

--- starter/balance/balance_invoker.py
+++ starter/balance/balance_invoker.py
@@ -1,26 +1,24 @@
 from balance.balance_commands import BalanceCommand
-from transaction.transaction import Transaction
+
 
 class BalanceInvoker():
     def __init__(self, logger=None):
         self._logger = logger
         self._history = []
-    
+
     def execute(self, command: BalanceCommand):
         self._conditional_log(f"Execute: {command.describe()}")
         command.execute()
         self._history.append(command)
         self._conditional_log(f"Complete: {command.describe()}")
-        
+
     def undo(self):
         if len(self._history) > 0:
             command = self._history.pop()
             self._conditional_log(f"Undo: {command.describe()}")
-            command.undo(command)
+            command.undo()
             self._conditional_log(f"Undo Complete: {command.describe()}")
 
     def _conditional_log(self, message):
-        print("BAM")
-        print(message)
         if (self._logger):
             self._logger.log(message)
--- starter/main.py
+++ starter/main.py
@@ -1,7 +1,10 @@
 """This module serves as the entry point for the program."""
 from balance.balance import Balance
+from balance.balance_commands import ApplyTransaction
+from balance.balance_invoker import BalanceInvoker
 from balance.balance_observer import LowBalanceAlertObserver
 from balance.balance_observer import PrintObserver
+from logger.logger import Logger
 from transaction.transaction import Transaction
 from transaction.transaction_category import TransactionCategory
 from transaction.transaction_adapter import TransactionAdapter
@@ -35,6 +38,18 @@ def main():
     # Apply all transactions to balance
     for transaction in all_transactions:
         balance.apply_transaction(transaction)
+
+    print("\nUsing command pattern...")
+    invoker = BalanceInvoker(Logger())
+    transaction1 = ApplyTransaction(balance, Transaction(3400, TransactionCategory.INCOME))
+    transaction2 = ApplyTransaction(balance, Transaction(78.41, TransactionCategory.EXPENSE))
+    transaction3 = ApplyTransaction(balance, Transaction(1078.41, TransactionCategory.EXPENSE))
+    invoker.execute(transaction1)
+    invoker.execute(transaction2)
+    invoker.undo()
+    invoker.execute(transaction3)
+    
+    
 
 
 if __name__ == "__main__":
